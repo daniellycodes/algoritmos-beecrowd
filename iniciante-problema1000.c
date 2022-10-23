5af16001c46a6405d6e28cc74bf731f8b8b5f8ed
Danielly Silva Paulino <98064933+daniellycodes@users.noreply.github.com>
2022-10-23 13:24:44 -0300

    Update iniciante-problema1000.c

--- iniciante-problema1000.c
+++ iniciante-problema1000.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int main() {
+
+ printf("Problema 1000 - Hello World!\n");
  
-    /* Enunciado:
-    O seu primeiro programa em qualquer linguagem de programação 
-    normalmente é o "Hello World!". Neste primeiro problema tudo 
-    o que você precisa fazer é imprimir esta mensagem na tela.*/
+ /* Enunciado:
+ O seu primeiro programa em qualquer linguagem de programação 
+ normalmente é o "Hello World!". Neste primeiro problema tudo 
+ o que você precisa fazer é imprimir esta mensagem na tela.*/
   
-    printf("Hello World!\n);
+ printf("Hello World!\n);
  
-    return 0;
+ return 0;
 }
